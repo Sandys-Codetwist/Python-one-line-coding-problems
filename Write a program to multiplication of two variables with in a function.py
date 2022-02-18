3401903a03e107a2a4d69c9d49a1196f3536d34b
Codetwist <99180218+Sandys-Codetwist@users.noreply.github.com>
2022-02-18 19:56:57 +0530

    Add files via upload

---
+++ Write a program to multiplication of two variables with in a function.py
@@ -0,0 +1,4 @@
+def multiplication(n):
+	return lambda a:a*n
+mult=multiplication(5)
+print(mult( 5))
---
+++ print even and odd numbers in a list.py
@@ -0,0 +1,5 @@
+list1=[10,2,4,6,87,5,66]
+numbers=[num for num in list1 if num%2==0 ]
+print(numbers)
+numbers=[num for num in list1 if num%2!=0 ]
+print(numbers)
